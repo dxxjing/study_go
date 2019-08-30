1313a1cc7f8f5436ffe24af0d2f1da9cd306bdd9
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-08-30 18:02:13 +0800

    本例采用结构体指针切片 且以指针作为接受者

--- src/listen14/sort2/sort2.go
+++ src/listen14/sort2/sort2.go
@@ -1,39 +1,53 @@
-type day struct {
-    num       int
-    shortName string
-    longName  string
-}
-//与sort区别 本例采用结构体指针切片 且以指针作为接受者
-type dayArray struct {
-    data []*day
-}
+package main
 
-func (p *dayArray) Len() int           { return len(p.data) }
-func (p *dayArray) Less(i, j int) bool { return p.data[i].num < p.data[j].num }
-func (p *dayArray) Swap(i, j int)      { p.data[i], p.data[j] = p.data[j], p.data[i] }
+import (
+	"fmt"
+	"sort"
+)
 
-func days() {
-    Sunday    := day{0, "SUN", "Sunday"}
-    Monday    := day{1, "MON", "Monday"}
-    Tuesday   := day{2, "TUE", "Tuesday"}
-    Wednesday := day{3, "WED", "Wednesday"}
-    Thursday  := day{4, "THU", "Thursday"}
-    Friday    := day{5, "FRI", "Friday"}
-    Saturday  := day{6, "SAT", "Saturday"}
-    data := []*day{&Tuesday, &Thursday, &Wednesday, &Sunday, &Monday, &Friday, &Saturday}
-    a := dayArray{data}
-    sort.Sort(&a)
-    if !sort.IsSorted(&a) {
-        panic("fail")
-    }
-    for _, d := range data {
-        fmt.Printf("%s ", d.longName)
-    }
-    fmt.Printf("\n")
+type User struct {
+	ID int
+	Score int
+	Name string
+}
+type UserArr []*User
+func (u *UserArr) Len() int {
+	return len(*u)
+}
+func (u *UserArr) Swap(i,j int) {
+	(*u)[i],(*u)[j] = (*u)[j],(*u)[i]
+}
+func (u *UserArr) Less(i,j int) bool {
+	return (*u)[i].ID < (*u)[j].ID
 }
 
-func main() {
-    ints()
-    strings()
-    days()
+//单字段排序
+//本例采用结构体指针切片 且以指针作为接受者
+
+func test2(){
+	var s []*User
+	s = []*User{
+		&User{11,34,"user11"},
+		&User{3,23,"user3"},
+		&User{34,11,"user34"},
+		&User{43,56,"user43"},
+		&User{67,44,"user67"},
+	}
+	tmp := UserArr(s)
+	fmt.Println("sort before:")
+	for _, d := range s {
+		fmt.Printf("%d %d %s,", d.ID,d.Score,d.Name)
+	}
+	fmt.Println()
+	//若结构体成员方法 内部显式的使用指针 则外部必须传指针进去
+	//且不能直接使用 &UserArr(s) 会提示无法获取地址
+	sort.Sort(&tmp) //升序
+	fmt.Println("sort after:")
+	for _, d := range s {
+		fmt.Printf("%d %d %s,", d.ID,d.Score,d.Name)
+	}
+}
+
+func main(){
+	test2()
 }
