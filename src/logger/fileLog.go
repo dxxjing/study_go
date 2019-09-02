413eb3cd6ba0ef913aaa1e40b4bcf980a73fcb0a
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-02 14:39:50 +0800

    Update fileLog.go

--- src/logger/fileLog.go
+++ src/logger/fileLog.go
@@ -15,9 +15,11 @@ type FileLog struct{
 
 }
 //构造函数 返回接口 LoggerInterface
-func NewFileLog(logLevel int,logPath,logName string) LoggerInterface {
+//todo 构造函数 logLevel参数 考虑要去除
+//func NewFileLog(logLevel int,logPath,logName string) LoggerInterface {
+func NewFileLog(logPath,logName string) LoggerInterface {
 	logger := &FileLog{
-		level : logLevel,
+		//level : logLevel,
 		logPath : logPath,
 		logName : logName,
 	}
@@ -30,6 +32,8 @@ func NewFileLog(logLevel int,logPath,logName string) LoggerInterface {
 func (f *FileLog) init(){
 	//目录需提前创建好 os.O_CREAT只会创建不存在的文件不会创建目录
 	//或者使用os.MkdirAll 进行创建
+	//todo 改造点1：warn以及warn以下的日志 根据调用文件、日期创建不同的logfile 如 test_20190902.log
+	//todo 改造点2: error及fatal单独创建一个errfile
 	fileName := fmt.Sprintf("%s%s.log",f.logPath,f.logName)
 	file,err := os.OpenFile(fileName,os.O_CREATE|os.O_APPEND|os.O_WRONLY,0777)
 	if err != nil {
@@ -53,21 +57,47 @@ func (f *FileLog) SetLevel(logLevel int){
 }
 
 func (f *FileLog) LogDebug(format string,args ...interface{}){
-	fmt.Fprintln(f.file,fmt.Sprintf(format,args...))
+	f.SetLevel(LogLevelDebug)
+	f.writeLog(format,args...)
 }
 
 func (f *FileLog) LogTrace(format string,args ...interface{}){
-	fmt.Fprintln(f.file,fmt.Sprintf(format,args...))
+	f.SetLevel(LogLevelTrace)
+	f.writeLog(format,args...)
 }
 func (f *FileLog) LogInfo(format string,args ...interface{}){
-	fmt.Fprintln(f.file,fmt.Sprintf(format,args...))
+	f.SetLevel(LogLevelInfo)
+	f.writeLog(format,args...)
 }
 func (f *FileLog) LogWarn(format string,args ...interface{}){
-	fmt.Fprintln(f.file,fmt.Sprintf(format,args...))
+	f.SetLevel(LogLevelWarn)
+	f.writeLog(format,args...)
 }
 func (f *FileLog) LogError(format string,args ...interface{}){
-	fmt.Fprintln(f.errFile,fmt.Sprintf(format,args...))
+	f.SetLevel(LogLevelError)
+	f.writeLog(format,args...)
 }
 func (f *FileLog) LogFatal(format string,args ...interface{}){
-	fmt.Fprintln(f.errFile,fmt.Sprintf(format,args...))
+	f.SetLevel(LogLevelFatal)
+	f.writeLog(format,args...)
+}
+
+func (f *FileLog) Close(){
+	f.file.Close()
+	f.errFile.Close()
+}
+
+//整合信息,写入日志文件
+func (f *FileLog) writeLog(format string,args ...interface{}){
+	nowTime := getNowDate()
+	levelName := getLogLevelName(f.level)
+	fileName,funcName,lineNo := getCallerInfo(3)
+	msg := fmt.Sprintf(format,args...)
+	logMsg := fmt.Sprintf("%s %s %s:%s[line:%d] :%s\n",nowTime,levelName,fileName,funcName,lineNo,msg)
+
+	logFile := f.file
+	if f.level == LogLevelError || f.level == LogLevelFatal {
+		logFile = f.errFile
+	}
+	fmt.Fprintf(logFile,logMsg)
 }
