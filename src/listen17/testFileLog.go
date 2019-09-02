97133b31b7aafdf901d738607a1042a4b622ffeb
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-02 19:56:00 +0800

    Update testFileLog.go

--- src/listen17/testFileLog.go
+++ src/listen17/testFileLog.go
@@ -8,9 +8,6 @@ func main(){
 
 	log := logger.NewFileLog()
 
-	//异步调用
-	go log.LogFatal("goroutine test fatal")
-
 	log.LogDebug("user id[%d] is come from china",123)
 	log.LogError("test error")
 	log.LogWarn("test warn log")
