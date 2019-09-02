d96ad62904775cc72d15fe36d11a4d208704cedb
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-02 19:54:09 +0800

    Update logger.go

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -15,26 +15,26 @@ func InitLog(logType string){
 
 //由于全局变量log 为私有的 定义方法
 
-func LogDebug(format string,args ...interface{}){
+func Debug(format string,args ...interface{}){
 	log.LogDebug(format,args...)
 }
 
-func LogTrace(format string,args ...interface{}){
+func Trace(format string,args ...interface{}){
 	log.LogTrace(format,args...)
 }
 
-func LogInfo(format string,args ...interface{}){
+func Info(format string,args ...interface{}){
 	log.LogInfo(format,args...)
 }
 
-func LogWarn(format string,args ...interface{}){
+func Warn(format string,args ...interface{}){
 	log.LogWarn(format,args...)
 }
 
-func LogError(format string,args ...interface{}){
+func Error(format string,args ...interface{}){
 	log.LogError(format,args...)
 }
 
-func LogFatal(format string,args ...interface{}){
+func Fatal(format string,args ...interface{}){
 	log.LogFatal(format,args...)
 }
