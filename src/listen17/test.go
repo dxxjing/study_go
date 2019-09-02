56daef65eb2090dfc65884735841310ea44ffb6f
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-02 15:08:07 +0800

    Update test.go

--- src/listen17/test.go
+++ src/listen17/test.go
@@ -9,7 +9,7 @@ func main(){
 	logPath := "e:/study_go/log/"
 	_ = os.MkdirAll(logPath,0777)
 	logName := "file_test"
-	fileLogger := logger.NewFileLog(0,logPath,logName)
+	fileLogger := logger.NewFileLog(logPath,logName)
 
 	fileLogger.LogDebug("user id[%d] is come from china",123)
 	fileLogger.LogError("test error")
