ddfd5413287e39c8966ee0677f60cc95227dcdb5
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-03 10:45:44 +0800

    Update consoleLog.go

--- src/logger/consoleLog.go
+++ src/logger/consoleLog.go
@@ -7,7 +7,7 @@ import (
 )
 //控制台日志  输出到控制台
 type ConsoleLog struct{
-	level int			//日志级别
+	//level int			//日志级别
 }
 //构造函数 返回接口 LoggerInterface
 func NewConsoleLog() LoggerInterface {
@@ -17,47 +17,47 @@ func NewConsoleLog() LoggerInterface {
 }
 
 //整合信息,写入日志文件
-func (f *ConsoleLog) writeLog(format string,args ...interface{}){
+func (f *ConsoleLog) writeLog(level int,format string,args ...interface{}){
 	nowTime := getNowDate()
-	levelName := getLogLevelName(f.level)
+	levelName := getLogLevelName(level)
 	fileName,funcName,lineNo := getCallerInfo(2)
 	funcName = path.Base(funcName)
 	msg := fmt.Sprintf(format,args...)
 	logMsg := fmt.Sprintf("%s %s %s:%s[line:%d] :%s\n",nowTime,levelName,fileName,funcName,lineNo,msg)
 	fmt.Fprintf(os.Stdout,logMsg)
 }
-
+/*
 func (f *ConsoleLog) SetLevel(logLevel int){
 	if logLevel < LogLevelDebug || logLevel > LogLevelFatal {
 		logLevel = LogLevelDebug
 	}
 	f.level = logLevel
 }
-
+*/
 func (f *ConsoleLog) LogDebug(format string,args ...interface{}){
-	f.SetLevel(LogLevelDebug)
-	f.writeLog(format,args...)
+	//f.SetLevel(LogLevelDebug)
+	f.writeLog(LogLevelDebug,format,args...)
 }
 
 func (f *ConsoleLog) LogTrace(format string,args ...interface{}){
-	f.SetLevel(LogLevelTrace)
-	f.writeLog(format,args...)
+	//f.SetLevel(LogLevelTrace)
+	f.writeLog(LogLevelTrace,format,args...)
 }
 func (f *ConsoleLog) LogInfo(format string,args ...interface{}){
-	f.SetLevel(LogLevelInfo)
-	f.writeLog(format,args...)
+	//f.SetLevel(LogLevelInfo)
+	f.writeLog(LogLevelInfo,format,args...)
 }
 func (f *ConsoleLog) LogWarn(format string,args ...interface{}){
-	f.SetLevel(LogLevelWarn)
-	f.writeLog(format,args...)
+	//f.SetLevel(LogLevelWarn)
+	f.writeLog(LogLevelWarn,format,args...)
 }
 func (f *ConsoleLog) LogError(format string,args ...interface{}){
-	f.SetLevel(LogLevelError)
-	f.writeLog(format,args...)
+	//f.SetLevel(LogLevelError)
+	f.writeLog(LogLevelError,format,args...)
 }
 func (f *ConsoleLog) LogFatal(format string,args ...interface{}){
-	f.SetLevel(LogLevelFatal)
-	f.writeLog(format,args...)
+	//f.SetLevel(LogLevelFatal)
+	f.writeLog(LogLevelFatal,format,args...)
 }
 
 
