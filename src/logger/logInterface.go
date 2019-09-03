6988deb62fa471ec99cb008620cf06ecf1728add
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-03 10:47:50 +0800

    Update logInterface.go

--- src/logger/logInterface.go
+++ src/logger/logInterface.go
@@ -2,8 +2,6 @@ package logger
 
 //日志接口
 type LoggerInterface interface{
-	SetLevel(level int) // 设置日志级别
-
 	LogDebug(format string,args ...interface{})
 	LogTrace(format string,args ...interface{})
 	LogInfo(format string,args ...interface{})
