ebb2b379eed1b9914f82384a8fbd5213a28f64d0
dxxjing <43433077+dxxjing@users.noreply.github.com>
2019-09-04 17:45:56 +0800

    父线程等待所有协程结束

--- src/listen20/chan_sync2.go
+++ src/listen20/chan_sync2.go
@@ -6,7 +6,7 @@ import (
 )
 
 //channel 实现父线程 等待所有子线程执行完再执行
-//todo 程序执行完会报错 fatal error: all goroutines are asleep - deadlock!
+
 func sonv11(done chan bool){
 
 	fmt.Println("sonv11 work...")
@@ -32,9 +32,17 @@ func main(){
 	go sonv11(done)
 	go sonv22(done)
 
+	//todo for range完毕 程序执行完会报错 fatal error: all goroutines are asleep - deadlock!
+	/*初步预测：由于主程序不知道何时停止从管道读取数据 且读取两次后 所有子协程都已结束，
+		主程序仍在等待从管道读取数据 导致死锁
+	*****解决办法：使用固定次数的for循环
 	for v := range done {
 		fmt.Println(v)
 	}
-
+	*/
+	//解决办法
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-done)
+	}
 	fmt.Println("all son work done")
 }
